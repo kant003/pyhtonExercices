175ac87568c19b18a0ea560bc0ad219b55c2b9f4
kant003 <gonzalezm.angel@gmail.com>
2022-10-25 14:52:27 +0200

    cambio

--- part2/eje10.py
+++ part2/eje10.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import math
 
-# Crea una función llamada collision, se le pasarán la velocidad de dos objetos (km/h) y la distancia que los separan (metros)
+# Crea una función llamada collision, se le pasarán la velocidad de dos objetos (km/h) y la distancia que los separan (km)
 # esta devolverá cuanto tiempo (en horas) tardarán en encontrarse
 # La formula que tienes que usar es tiempo(s) = distancia(m) / (velocidad1 (m/s) +velocidad2 (m/s))
 
