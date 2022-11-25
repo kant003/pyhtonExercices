db279ff093115f375a46c72e7484cfec54961ba2
kant003 <gonzalezm.angel@gmail.com>
2022-11-25 14:05:42 +0100

    correc

--- part9/eje05.py
+++ part9/eje05.py
@@ -6,11 +6,11 @@ def suma_de_digitos_iterativa(num):
     pass
 
 def test():
-    suma_de_digitos_iterativa(0) == 0, "0 should be 0"
-    suma_de_digitos_iterativa(5) == 5, "5 should be 5"
-    suma_de_digitos_iterativa(2424) == 3, "2424 should be 3"
-    suma_de_digitos_iterativa(458759545485) == 6, "458759545485 should be 6"
-    suma_de_digitos_iterativa(75124365485544) == 9, "75124365485544 should be 9"
+    assert suma_de_digitos_iterativa(0) == 0, "0 should be 0"
+    assert suma_de_digitos_iterativa(5) == 5, "5 should be 5"
+    assert suma_de_digitos_iterativa(2424) == 3, "2424 should be 3"
+    assert suma_de_digitos_iterativa(458759545485) == 6, "458759545485 should be 6"
+    assert suma_de_digitos_iterativa(75124365485544) == 9, "75124365485544 should be 9"
 
 if __name__ == "__main__":
     test()
